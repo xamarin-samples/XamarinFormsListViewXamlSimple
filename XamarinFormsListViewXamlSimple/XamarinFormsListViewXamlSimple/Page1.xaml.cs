6eaaa307275401175cf038b034d2d0395a34d6a2
kobake <kobake@users.sourceforge.net>
2016-08-11 14:04:03 +0900

    Put ListView onto XAML page, and bind string list.

--- XamarinFormsListViewXamlSimple/XamarinFormsListViewXamlSimple/Page1.xaml.cs
+++ XamarinFormsListViewXamlSimple/XamarinFormsListViewXamlSimple/Page1.xaml.cs
@@ -13,6 +13,16 @@ namespace XamarinFormsListViewXamlSimple
 		public Page1()
 		{
 			InitializeComponent();
+
+			// Make string list
+			List<string> list = new List<string>();
+			for(int i = 0; i < 20; i++)
+			{
+				list.Add(string.Format("item-{0}", i));
+			}
+
+			// Bind
+			this.BindingContext = list;
 		}
 	}
 }
